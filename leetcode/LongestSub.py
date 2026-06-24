1b46386e9afef8100db8d9e90fe3b59a4bdec948
Jay Pradeep Ukarde <jayukarde10@gmail.com>
2026-06-25 00:52:49 +0530

    wrong

--- leetcode/LongestSub.py
+++ leetcode/LongestSub.py
@@ -1,7 +1,26 @@
-s="fewfewf"
-left=0
-right=0
-while left<=len(s):
-    print(left,right)
-    left+=1
-    right+=1
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        left=0
+        right=0
+        r=""
+        longsum=0
+        sub=0
+        while right<len(s):
+            if s[left] not in r:
+                r+=s[right]
+                sub+=1
+                right+=1
+                longsum=max(sub,longsum)
+            elif s[left]  in r:
+                if left==right:
+                    r=s[right]
+                    sub+=1
+                    right+=1
+                    longsum=max(sub,longsum)
+                elif left<right:
+                    sub=0
+                    left+=1
+                    r=r[1:]
+            
+        longsum=max(sub,longsum)
+        return longsum
