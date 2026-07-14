1f213d15d7b2fbf6becb13a03ce71d994520f6fd
Jay Pradeep Ukarde <jayukarde10@gmail.com>
2026-07-15 00:12:03 +0530

    runlengthcode

--- string/runlength.py
+++ string/runlength.py
@@ -1,18 +1,41 @@
-s="aaaabbbbccaa"
-d={}
+s="abbcaa"
 left=0
-count=0
-se=[]
-while left<len(s):
-    if s[left]  in se:
-        count+=1  
-        d[se[0]]=count  
+right=1
+count=1
+result=""
+while left<len(s)-1:
+    if s[left]==s[right]:
+        count+=1                    #Jay Logic is right but fail for null string only 
+        left+=1
+        right+=1
+        
     else:
-        se=[]
-        se.append(s[left])
-        count=0
-        count+=1
-        d[se[0]]=count
-    left+=1
+        result+=s[left]+str(count)
+        count=1
+        left+=1
+        right+=1
+result+=s[right-1]+str(count)
 
-print(d)
+print(result)
+
+#perfect
+s = "abbcaa"
+
+if not s:
+    print("")
+else:
+    result = ""
+    count = 1
+
+    for i in range(1, len(s)):
+
+        if s[i] == s[i - 1]:
+            count += 1
+        else:
+            result += s[i - 1] + str(count)
+            count = 1
+
+    # Add the last group
+    result += s[-1] + str(count)
+
+    print(result)
