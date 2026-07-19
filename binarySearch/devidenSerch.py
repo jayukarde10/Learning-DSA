87bd7fb06233be1eae43ff6c5919847438dfbaf8
Jay Pradeep Ukarde <jayukarde10@gmail.com>
2026-07-19 23:29:08 +0530

    new B

---
+++ binarySearch/devidenSerch.py
@@ -0,0 +1,22 @@
+arr = [3, 7, 10, 15, 20, 24, 31, 42, 56]
+target=32
+left = 0
+right = len(arr) - 1
+
+while left <= right:
+    mid =(left+right)//2 # instead of int((left+right)/2)
+    if arr[mid] == target:
+        print("found at",mid)
+        break
+    elif arr[mid] < target:
+        left=mid+1
+    else:
+        right=mid-1
+else:
+    print("not found")
+        
+
+        
+        
+    
+
